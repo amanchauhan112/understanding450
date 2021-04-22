3e294e886d3e741a2c75953b254740db9870b203
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-04-22 20:30:06 +0530

    added route for loginlogout register

--- server/router/routes.js
+++ server/router/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const CommentModel=require('../model/comment_schema')
+const RegisterModel=require('../model/register_schema')
 
 router.get('/',(req,res)=>{
     CommentModel.find({ })
@@ -47,5 +48,80 @@ router.post('/save',(req,res)=>{
     
 
 });
+
+router.get('/register',(req,res)=>{
+    RegisterModel.find()
+    .then((data)=>{
+           res.json(data);
+        })
+    .catch((error)=>{
+        console.log('error:' ,error)
+        res.send(error);
+    });
+
+});
+
+router.post('/register',async (req,res)=>{
+    
+    try {
+        const password = req.body.password;
+        const password2 = req.body.password2;
+        const email = req.body.email;
+    
+        if (password === password2) {
+          const user = new RegisterModel(req.body);
+          const userExist = await RegisterModel.findOne({ email: email });
+          if (userExist) {
+            res.status(400).send("user already exist");
+          } else {
+            user
+              .save()
+              .then((result) => {
+                res.status(200).send("registered");
+              })
+              .catch((err) => {
+                res.status(400).send("/register error");
+                console.log("registration error");
+              });
+          }
+        } else {
+          res.status(400).send("password does not match");
+        }
+    
+        // console.log(req.body);
+      } catch (error) {
+        res.status(401).send(error);
+      }
+
+});
+
+
+router.post('/login',async(req,res)=>{
+    const userEmail = req.body.email;
+    const userPass = req.body.password;
+    try{
+    const userExist = await RegisterModel.findOne({email: userEmail});
+          if(userExist.password === userPass){
+            let token = await userExist.generateAuthToken();
+            res.cookie("jwt",token,{
+              httpOnly:true,
+            });
+            res.status(200).send("logged in");
+          }
+          else {
+            res.status(400).send("Incorrect Password");
+          }
+    }
+    catch{
+      res.status(500).send("user not found")
+    }
+  });
+  
+  router.post('/logout',async (req,res)=>{
+    res.clearCookie('jwt',{path:'/'});
+    res.status(200).send('logout');
+  });
+  
+  
 
 module.exports = router;
