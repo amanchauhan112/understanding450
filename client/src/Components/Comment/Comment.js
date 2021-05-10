2da1b1df2c58edf25c46899af32f1c9493e7ce2f
Aman <amanchauhan2801@gmail.com>
2021-05-11 02:03:35 +0530

    comment section improvement

--- client/src/Components/Comment/Comment.js
+++ client/src/Components/Comment/Comment.js
@@ -1,125 +1,15 @@
-import React,{Component} from 'react'
-import axios from 'axios';
-import './Comment.css';
-class Comment extends Component {
-      
-    state={
-        comment:'',
-        posts:[],
-    };
+import { AllComment } from "./AllComment";
+import Comment2 from "./Comment2";
 
-    componentDidMount=()=>{
-        this.getComment();
-    }
-    componentDidUpdate=()=>{
-this.getComment();
-    }
-    
-
-    getComment=()=>{
-        axios.get('http://localhost:8080/api')
-
-        .then((response)=>{
-          const data = response.data;
-          this.setState({posts:data})
-        // console.log('Data Recieved');
-       
-        })
-       .catch(()=>{
-           alert('Error retireiving data!!');
-       });
-    }
-
-    displayComment = (posts) =>{
-
-        if(!posts.length) return null;
-
-        return posts.map((post,index)=>(
-
-            <div key={index}>
-            <h4>{post.comment}</h4>
-
-            </div>
-        ));
-    };
-
-    handleChange=(event)=>{
-        const target=event.target;
-        const name=target.name;
-        const value=target.value;
-
-        this.setState({
-            [name]:value
-        });
-
-    }
-    
-    resetUserInput=()=>{
-
-        this.setState({
-            comment:'',
-        });
-    };
-
-    submit=(event) =>{
-
-        event.preventDefault();
-
-        console.log(this.state);
-
-        const payload={
-            comment:this.state.comment,
-            
-        };
-
-        axios({
-            url:'http://localhost:8080/api/save',
-            method:'POST',
-            data: payload
-        })
-        .then(()=>{
-            console.log('Data Recieved');
-            this.resetUserInput();
-        })
-        .catch(()=>{
-            console.log('Error from axios method')
-        })
-
-    };
-
-    render(){
-
+const Comment= ()=> {
   
-
-    return (
-        <div>
-            <form onSubmit={this.submit}>
-
-             <input
-              type="text" 
-              name="comment"
-              required="true"
-              id="comment"
-              placeholder="Comment..."
-              value={this.state.comment}
-              onChange={this.handleChange}
-              />
-
-              <button >Comment</button>
-              </form>
-              
-<p style={{fontWeight:'bold'}}>Comments:({this.state.posts.length})</p>
-
-<hr/>
-<div>
-    {this.displayComment(this.state.posts)}
-</div>
-        </div>
-    )
+  return (<>
+          
+            <Comment2/>  
+            <AllComment/>
+        
+        </>
+  );
 }
 
-
-
-}
-
-export default Comment
+export default Comment;
--- server/model/comment_schema.js
+++ server/model/comment_schema.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
+// const {ObjectId}=mongoose.Schema.Types
 
 //Schema
 
 const Schema=mongoose.Schema;
 
 const Comment = new Schema ({
+    userID:{
+        type: String,
+     },
     comment : 
     {
         type:String,
-    
         pageId:'',
-    }
+    },
+    // postedBy:{
+    //     type:ObjectId,
+    //     ref:"User"
+    // }
    
 
 },
