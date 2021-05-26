89d4c78e2beda4bd14a87e95c5d743cf65650970
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-05-26 19:49:40 +0530

    cleared banner warning

--- client/src/Containers/Banner/Banner.js
+++ client/src/Containers/Banner/Banner.js
@@ -4,8 +4,9 @@ function Banner({ children, title, subTitle }) {
   return (
     <div className="banner">
       <h1>{title}</h1>
-      <h2>{subTitle}</h2>
-      <p>{children}</p>
+      <h1>{subTitle}</h1>
+
+     {children}
     </div>
   );
 }
