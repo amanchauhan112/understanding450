3fd0788e52627c21b8ca44dfda4156fc07edc1ba
Aman Chauhan <amanchauhan2801@gmail.com>
2021-06-26 23:33:34 +0530

    css and LL

--- client/src/Components/pages/linkedlist.js
+++ client/src/Components/pages/linkedlist.js
@@ -9,6 +9,73 @@ const LinkedList = () => {
       </div>
 
 <br/>
+<div className="question">
+<h1><a href="https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1" className="q">1.Write a Program to reverse the Linked List.</a></h1>
+<a href="ll_1" className="answer_link">&emsp; Link to answer</a>
+
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1" className="q">2.Reverse a Linked List in group of Given Size. [Very Imp]
+</a></h1>
+<a href="ll_2" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/detect-loop-in-linked-list/1" className="q">3.Write a program to Detect loop in a linked list.
+</a></h1>
+<a href="ll_3" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/remove-loop-in-linked-list/1" className="q">4.Write a program to Delete loop in a linked list.
+</a></h1>
+<a href="ll_4" className="answer_link">&emsp; Link to answer</a>
+
+{/* <h1><a href="" className="q"></a></h1>
+<a href="" className="answer_link">&emsp; Link to answer</a>
+ */}
+
+<h1><a href="https://www.geeksforgeeks.org/find-first-node-of-loop-in-a-linked-list/" className="q">5.Find the starting point of the loop. 
+</a></h1>
+<a href="ll_5" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/remove-duplicate-element-from-sorted-linked-list/1" className="q">6.Remove Duplicates in a sorted Linked List.
+</a></h1>
+<a href="ll_6" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/remove-duplicates-from-an-unsorted-linked-list/1" className="q">7.Remove Duplicates in a Un-sorted Linked List.
+</a></h1>
+<a href="ll_7" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://www.geeksforgeeks.org/move-last-element-to-front-of-a-given-linked-list/" className="q">8.Write a Program to Move the last element to Front in a Linked List.
+</a></h1>
+<a href="ll_8" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/add-1-to-a-number-represented-as-linked-list/1" className="q">9.Add “1” to a number represented as a Linked List.
+</a></h1>
+<a href="ll_9" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/add-two-numbers-represented-by-linked-lists/1" className="q">10.Add two numbers represented by linked lists.
+</a></h1>
+<a href="ll_10" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/intersection-of-two-sorted-linked-lists/1" className="q">11.Common Elements in two sorted Linked List</a></h1>
+<a href="ll_11" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/intersection-point-in-y-shapped-linked-lists/1#" className="q">12.Intersection point of Y shaped Linked List</a></h1>
+<a href="ll_12" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/sort-a-linked-list/1" className="q">13.Merge Sort For Linked lists.[Very Important]
+</a></h1>
+<a href="ll_13" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/quick-sort-on-linked-list/1" className="q">14.Quick Sort in Linked List</a></h1>
+<a href="ll_14" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://leetcode.com/problems/middle-of-the-linked-list/" className="q">15.Middle Element of Linked List</a></h1>
+<a href="ll_15" className="answer_link">&emsp; Link to answer</a>
+
+<h1><a href="https://practice.geeksforgeeks.org/problems/circular-linked-list/1" className="q">16.Check if Linked List is circular or not.</a></h1>
+<a href="ll_16" className="answer_link">&emsp; Link to answer</a>
+
+
+</div>
+
 
     </div>
   );
